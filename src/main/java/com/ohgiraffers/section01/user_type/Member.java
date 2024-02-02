5ae56276b5eec770400486cf3ea27a67e67b69d2
minseo <juhee0629032@gmail.com>
2024-02-02 16:21:13 +0900

    commit

---
+++ src/main/java/com/ohgiraffers/section01/user_type/Member.java
@@ -0,0 +1,14 @@
+package com.ohgiraffers.section01.user_type;
+
+public class Member {
+
+    // 이것을 전역 변수라고 하고, (필드 == 인스턴스변수 == 속성) 라고 부른다.
+    String id;  // 클래스안에 있는 변수, 클래스변수, 전역변수
+    String pwd;
+    String name;
+    int age;
+    char gender;
+    String[] hobby;     // 회원 정보라는 클래스를 만든 것.
+
+
+}
